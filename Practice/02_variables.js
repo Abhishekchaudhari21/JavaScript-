98e9421f653440fa79c637ff6ad0ddb1b84f0269
Abhishek Chaudhari <143903535+Abhishekchaudhari21@users.noreply.github.com>
2025-08-01 05:44:05 +0000

    datatypes

---
+++ Practice/02_variables.js
@@ -0,0 +1,14 @@
+// number => 2 or 3
+// bigint - used when we need big value of int.  e.g. stock market
+// string => ""   mostly preffered in "".
+// boolean => true/ false
+// null => standalone value
+// undefined => value not assigned
+// symbol => for unique data
+
+
+// object
+
+console.log(typeof null);   //null
+console.log(typeof undefined);   //undefined
+
